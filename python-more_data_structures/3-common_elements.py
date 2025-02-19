5b926dcaedc4e659a1f8684729a51812fb10e863
TRolande <r.tumugane@alustudent.com>
2025-02-19 08:11:45 -0800

    sc

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# a function that returns a set of common elements in two sets.
 def common_elements(set_1, set_2):
-    set_3 = set_1.intersection(set_2)
-    return set_3
+    return set_1 & set_2
