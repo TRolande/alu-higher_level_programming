4aa06280f23e77278e7c51a5bafa4e262c08521b
TRolande <r.tumugane@alustudent.com>
2025-02-19 08:42:44 -0800

    sc

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-# function that returns the weighted average
-# of all integers tuple (<score>, <weight>)
-def weight_average(my_list=[]):
-    count = len(my_list)
-    if count == 0:
+def to_subtract(num_list):
+    max_num = max(num_list)
+    return max_num - sum(n for n in num_list if max_num > n)
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
-    sum = 0
-    weighted_sum = 0
-    for rows in my_list:
-        multiply = 1
-        for i in rows:
-            multiply *= i
-        sum += multiply
-    for i in range(0, count):
-        weighted_sum += my_list[i][1]
-    w_average = sum / weighted_sum
-    return w_average
+    rm = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num, last_rom, num_list = 0, 0, [0]
+    for char in roman_string:
+        if char in rm:
+            if rm[char] <= last_rom:
+                num += to_subtract(num_list)
+                num_list = [rm[char]]
+            else:
+                num_list.append(rm[char])
+            last_rom = rm[char]
+    return num + to_subtract(num_list)
