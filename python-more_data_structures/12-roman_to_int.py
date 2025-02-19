da8df9d7bd40b55c4e5ce2b3af91a594fbf13092
TRolande <r.tumugane@alustudent.com>
2025-02-19 08:40:07 -0800

    sc

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-# Create a function that converts a Roman numeral to an integer.
+def to_subtract(num_list):
+    max_num = max(num_list)
+    return max_num - sum(n for n in num_list if max_num > n)
+
+
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) and roman_string is not None:
-        count = len(roman_string)  # lenght of the input string
-
-        i = 0  # iteration variable
-
-        tmp = 0  # temporary variable to store numberals like iv, ix, e.t.c
-
-        result = 0  # holds final result
-        latin = list(roman_string)
-        rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        while i < count:
-            next_index = i + 1
-            if next_index == count or rom[latin[i]] >= rom[latin[next_index]]:
-                result += rom[latin[i]]
-                i += 1
-            elif count > 1 and rom[latin[i]] < rom[latin[next_index]]:
-                tmp += rom[latin[next_index]] - rom[latin[i]]
-                i += 2
-        result = result + tmp
-        return result
-    else:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
+    rm = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num, last_rom, num_list = 0, 0, [0]
+    for char in roman_string:
+        if char in rm:
+            if rm[char] <= last_rom:
+                num += to_subtract(num_list)
+                num_list = [rm[char]]
+            else:
+                num_list.append(rm[char])
+            last_rom = rm[char]
+    return num + to_subtract(num_list)
