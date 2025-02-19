390ad875ec263981c1d640ccbb0b640371aca11e
TRolande <r.tumugane@alustudent.com>
2025-02-19 08:37:55 -0800

    sc

--- python-more_data_structures/11-multiply-list-map.py
+++ python-more_data_structures/11-multiply-list-map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-# function that returns a key with the biggest integer value.
-def best_score(a_dictionary):
-    if a_dictionary == {} or a_dictionary is None:
-        return None
-    values = a_dictionary.values()
-    max_value = max(values)
-    keys = a_dictionary.keys()
-    for keys in a_dictionary:
-        if a_dictionary[keys] == max_value:
-            return keys
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
