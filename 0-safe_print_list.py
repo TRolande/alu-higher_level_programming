c925d67695c32062b39ad3286e3936e40ac507b7
TRolande <r.tumugane@alustudent.com>
2025-02-28 14:23:06 -0800

    python

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+# Write a function that prints x elements of a list.
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    if type(x) != int:
+        raise ValueError("is not an integer")
+    while count < x:
+        try:
+            print(my_list[count], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
