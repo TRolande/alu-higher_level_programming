999040be859bd96aec5022a2c9235703c176718e
TRolande <r.tumugane@alustudent.com>
2025-03-03 02:16:20 -0800

    python

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-# Write a function that prints x elements of a list.
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    if type(x) != int:
-        raise ValueError("is not an integer")
-    while count < x:
+    num = 0
+    for i in range(x):
         try:
-            print(my_list[count], end="")
-            count += 1
+            print(my_list[i], end="")
+            num += 1
         except IndexError:
             break
     print()
-    return count
+    return num
