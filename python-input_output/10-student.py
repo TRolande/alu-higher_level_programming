e025f311eb7db03e8b67ac46d106ee12963d192f
TRolande <r.tumugane@alustudent.com>
2025-03-08 14:28:50 -0800

    this project will wor about inputing and outputing

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""a script that creates a class"""
+"""Defines a class Student."""
 
 
 class Student:
-    """a class called student that has the following atttributes
-        Attributes:
-            first_name
-            last_name
-            age
-            _json: retrieves a dictionary of the instance attributes
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, lis=[2]):
-        if isinstance(lis, list):
-            if lis[0] == 2:
-                return self.__dict__
-            if len(lis) == 0:
-                self.__dict__.clear()
-                return self.__dict__
-            dic = self.__dict__
-            new = {}
-            for i in lis:
-                if i in dic:
-                    value = dic[i]
-                    new[i] = value
-        return new
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
